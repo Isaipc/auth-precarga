0df2f77944941c980306dee0ea59cd72fc4475ea
Isai Polito <ispolitoc@gmail.com>
2021-02-15 23:57:41 -0600

    add MateriaPrecarga model

---
+++ app/Models/MateriaPrecarga.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MateriaPrecarga extends Model
+{
+    use HasFactory;
+
+    protected $table = 'insprecarga';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'aluctr',
+        'periodo',
+        'matcve',
+        'matcre',
+        'tipo',
+    ];
+}
