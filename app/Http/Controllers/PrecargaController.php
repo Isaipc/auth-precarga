52383efb0329658567f04dfb892bfa378b516371
Isai Polito <ispolitoc@gmail.com>
2021-02-15 23:58:58 -0600

    add conditional, update user model

--- app/Http/Controllers/PrecargaController.php
+++ app/Http/Controllers/PrecargaController.php
@@ -2,39 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MateriaPrecarga;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use stdClass;
-
-define('RETICULA', 'd');
 class PrecargaController extends Controller
 {
+    protected $max_creditos = 0;
+    protected $max_materias = 0;
 
     /**
      * Solicita las materias precargadas del alumno
      */
     public function obtenerPrecarga(Request $request)
     {
-        $materias = DB::table('insprecarga')
-            ->join('insdmater', 'insdmater.matcve', '=', 'insprecarga.matcve')
-            ->where('insprecarga.aluctr', $request->user()->login)
-            ->select(
-                'insprecarga.id',
-                'insprecarga.grupo AS grupo',
-                'insprecarga.periodo AS periodo',
-                'insprecarga.matcve AS clave',
-                'insdmater.matnom AS nombre',
-                'insdmater.matnco AS nombre_corto',
-                'insdmater.mathte AS teoricas',
-                'insdmater.mathpr AS practicas',
-                'insdmater.matcre AS creditos',
-            )
-            ->orderBy('insprecarga.periodo')
-            ->get();
-
+        $materias = $request->user()->materiasPrecargadas()->get();
 
         return response()->json([
-            'total_materias' => $materias->count(),
             'materias' => $materias,
         ]);
     }
@@ -44,62 +26,36 @@ class PrecargaController extends Controller
      */
     public function obtenerMaterias(Request $request)
     {
+        // Se extrae el usuario del alumno:
+        $user = $request->user();
+        // Se extraen las materias del periodo disponibles
+        $materias = $user->materiasPeriodo()->get();
+        // Se extraen las materias especiales del alumno de la bd:
+        $especiales = $user->materiasEspeciales()->get();
+        // Se extraen las materias reprobadas del alumno de la bd:
+        $repites = $user->materiasRepites()->get();
 
-        // se obtienen las materias cursadas por el alumno
-        $materias_cursadas = DB::table('insdlista')
-            ->select('matcve AS clave')
-            ->where('aluctr', $request->user()->login)
-            ->where('liscal', '>', 0)
-            ->get();
+        // **CONDICIONES**
+        // Alumnos irregulares:
+        if ($repites->count() > 0)
+            $materias = $materias->merge($repites);
 
+        // Alumnos con 1 especial:
+        if ($especiales->count() == 1) {
+            // Se excluye RESIDENCIAS PROFESIONALES
+            // Se incluye el especial
+            $materias = $materias
+                ->where('clave', '<>', 'RISC1001')
+                ->merge($especiales);
 
-        $arr = array();
-        foreach ($materias_cursadas as $key => $value) {
-            array_push($arr, $value->clave);
-        }
+            // Alumnos con 2 especiales:
+        } else if ($especiales->count() >= 2)
 
-        // Se extraen los periodos del reticula
-        $reticula = DB::table('insdretic')
-            ->select('retper AS periodo')
-            ->whereRaw('placve = BINARY ?', RETICULA)
-            ->orderBy('periodo')
-            ->distinct()
-            ->get();
-
-
-
-        // Se recorren los periodos
-        foreach ($reticula as $key => $value) {
-
-            // Se extraen las materias de un periodo especifico
-            $materias = DB::table('insdretic')
-                ->join('insdmater', 'insdmater.matcve', '=', 'insdretic.matcve')
-                ->select(
-                    'insdretic.id',
-                    'insdretic.retper AS periodo',
-                    'insdretic.matcve AS clave',
-                    'insdmater.matnom AS nombre',
-                    'insdmater.matnco AS nombre_corto',
-                    'insdmater.mathte AS teoricas',
-                    'insdmater.mathpr AS practicas',
-                    'insdmater.matcre AS creditos',
-                    DB::raw("'N' AS tipo"),
-
-                )
-                ->whereRaw('placve = BINARY ?', RETICULA)
-                ->where('insdretic.retper', $value->periodo)
-                ->whereNotIn('insdretic.matcve', $arr)
-                ->get();
-
-            $value->total_materias = $materias->count();
-
-            $value->materias = $materias;
-        }
-
+            // Se incluyen solo los especiales
+            $materias = $especiales;
 
         return response()->json([
-            'num_periodos' => $reticula->count(),
-            'reticula' => $reticula,
+            'materias' => $materias,
         ]);
     }
 
@@ -108,34 +64,72 @@ class PrecargaController extends Controller
      */
     public function guardar(Request $request)
     {
-
+        // Se valida la existencia de la lista de materias:
         $request->validate([
             'materias' => 'required'
         ]);
 
-        $num_control = $request->user()->login;
+        // Se extrae el usuario del alumno:
+        $user = $request->user();
 
-        DB::table('insprecarga')
-            ->where('insprecarga.aluctr', $num_control)
-            ->delete();
+        //Se extraen las materias subidas:
+        $materias = $request->materias;
 
-        $materias = array();
 
-        foreach ($request->materias as $key => $value) {
-            
-            $_materia = [
-                'aluctr' =>  $num_control,
+        $this->validarPrecarga($user, $materias);
+
+        // Limpiar las precargas del alumno:
+        MateriaPrecarga::where('aluctr', $user->login)->delete();
+
+        foreach ($materias as $value) {
+            MateriaPrecarga::create([
+                'aluctr' =>  $user->login,
                 'periodo' =>  $value['periodo'],
                 'matcve' =>  $value['clave'],
                 'matcre' =>  $value['creditos'],
+                'tipo' =>  $value['tipo'],
                 // 'grupo' => '',
-            ];
-
-            array_push($materias, $_materia);
-        }    
-        
-        DB::table('insprecarga')->insert($materias);
+            ]);
+        }
 
         return response()->json(['message' => 'Precarga finalizada'], 201);
+    }
+
+    protected function validarPrecarga($user, $materias)
+    {
+        $total_creditos = 0;
+
+        // Se extraen las materias especiales del alumno de la bd:
+        $especiales = $user->materiasEspeciales();
+        // Se extraen las materias reprobadas del alumno de la bd:
+        $repites = $user->materiasRepites();
+
+        if ($repites->get('matcve')->count() > 0) {
+            $this->max_creditos = 36;
+            $this->max_materias = 7;
+        }
+
+        if ($especiales->get()->count() == 1) {
+            $this->max_creditos = 22;
+            $this->max_materias = 4;
+        }
+
+        // Calcula el total de créditos de las materias subidas
+        foreach ($materias as $key => $materia)
+            $total_creditos += $materia['creditos'];
+
+        if ($total_creditos > $this->max_creditos)
+            return response()->json(
+                ['message' => 'No puede exceder el límite de créditos: ' .
+                    $this->max_creditos],
+                422
+            );
+
+        if (count($materias) > $this->max_materias)
+            return response()->json(
+                ['message' => 'No puede exceder el límite de materias: ' .
+                    $this->max_materias],
+                422
+            );
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
 use Laravel\Passport\HasApiTokens;
 
+define('RETICULA', 'd');
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens;
@@ -47,8 +49,77 @@ class User extends Authenticatable
         // 'email_verified_at' => 'datetime',
     ];
 
-    public function details()
+    /* Consulta las materias del periodo que debe cursar el alumno */
+    public function materiasPeriodo()
     {
+        // Se extraen las materias del periodo que tiene que cursar el alumno
+        return $this->materias('N')
+            ->where('insdretic.retper', $this->alumno->nvoper);
+    }
+
+    /* Consulta las materias precargadas del alumno*/
+    public function materiasPrecargadas()
+    {
+        return DB::table('insprecarga')
+            ->join('insdmater', 'insdmater.matcve', '=', 'insprecarga.matcve')
+            ->where('insprecarga.aluctr', $this->login)
+            ->select(
+                'insprecarga.id',
+                'insprecarga.grupo AS grupo',
+                'insprecarga.periodo AS periodo',
+                'insprecarga.matcve AS clave',
+                'insdmater.matnom AS nombre',
+                'insdmater.matnco AS nombre_corto',
+                'insdmater.mathte AS teoricas',
+                'insdmater.mathpr AS practicas',
+                'insdmater.matcre AS creditos',
+                'insprecarga.tipo AS tipo',
+            )
+            ->orderBy('insprecarga.periodo');
+    }
+
+    /* Consulta las materias especiales del alumno */
+    public function materiasEspeciales()
+    {
+        return $this->materias('E')
+            ->whereIn('insdretic.matcve', function ($query) {
+                $query->select('matcve')
+                    ->from('insespeciales')
+                    ->where('aluctr', $this->login);
+            });
+    }
+
+    /* Consulta las materias reprobadas del alumno */
+    public function materiasRepites()
+    {
+        // Se puede solucionar con una tabla 🤔
+        return $this->materias('R')
+            ->where('insdretic.retper', $this->alumno->nvoper)
+            ->whereIn('insdretic.matcve', function ($query) {
+                $query->select('matcve')
+                    ->from('insdlista')
+                    ->where('aluctr', $this->login)
+                    ->where('liscal', '<', 70);
+            })
+            ->whereNotIn('insdretic.matcve', function ($query) {
+                $query->select('matcve')
+                    ->from('insdlista')
+                    ->where('aluctr', $this->login)
+                    ->where('liscal', '>=', 70);
+            })
+            ->whereNotIn('insdretic.matcve', function ($query) {
+                $query->select('matcve')
+                    ->from('insespeciales')
+                    ->where('aluctr', $this->login);
+            });
+    }
+
+    /* Consulta que devuelve los detalles del alumno según sus datos académicos */
+    public function detallesDelAlumno()
+    {
+        $especiales = $this->materiasEspeciales()->get('matcve');
+        $repites = $this->materiasRepites()->get('matcve');
+
         return DB::table('insdclist')
             ->join('inscarreras', 'inscarreras.id', '=', 'insdclist.carcve')
             ->join('inscaptur', 'inscaptur.login', '=', 'insdclist.aluctr')
@@ -64,8 +135,34 @@ class User extends Authenticatable
                 'insdclist.promedio',
                 'insdclist.creditos',
                 'insdclist.registro',
-                'insdclist.inscrito'
+                'insdclist.inscrito',
+                DB::raw($repites->count() . ' AS repites'),
+                DB::raw($especiales->count() . ' AS especiales')
             )
             ->first();
+    }
+
+    public function alumno()
+    {
+        return $this->hasOne(Alumno::class, 'aluctr', 'login');
+    }
+
+
+    public function materias($tipo)
+    {
+        return DB::table('insdretic')
+            ->join('insdmater', 'insdmater.matcve', '=', 'insdretic.matcve')
+            ->select(
+                'insdretic.id',
+                'insdretic.retper AS periodo',
+                'insdretic.matcve AS clave',
+                'insdmater.matnom AS nombre',
+                'insdmater.matnco AS nombre_corto',
+                'insdmater.mathte AS teoricas',
+                'insdmater.mathpr AS practicas',
+                'insdmater.matcre AS creditos',
+                DB::raw("'" . $tipo . "' AS tipo"),
+            )
+            ->whereRaw('placve = BINARY ?', RETICULA);
     }
 }
